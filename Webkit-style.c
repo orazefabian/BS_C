1d87ab3ab6fa117c0574876396b48350e5ee4c80
fabian <oraze.fabian@icloud.com>
2020-03-07 12:37:49 +0100

    added styles clang

---
+++ Webkit-style.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAXLEN 10
+
+void printArr(int arr[], int index)
+{
+    for (int i = index; i < MAXLEN; ++i) {
+        printf("Fibonacci at index: [%d] = %d\n", i, arr[i]);
+    }
+}
+
+int main(int argc, char const* argv[])
+{
+    srand(time(0));
+    int arrfib[MAXLEN];
+    arrfib[0] = 0;
+    arrfib[1] = 1;
+
+    for (int i = 2; i < MAXLEN; ++i) {
+        arrfib[i] = arrfib[i - 1] + arrfib[i - 2];
+    }
+
+    int start = rand() % 9;
+    printf("Random start point = %d\n", start);
+    printArr(arrfib, start);
+
+    return 0;
+}
