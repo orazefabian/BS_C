83ae97be265f5ba8262890c4a5b577e1dc9ccb2d
fabian <oraze.fabian@icloud.com>
2020-03-07 10:36:13 +0100

    completed array.c task

--- array.c
+++ array.c
@@ -2,30 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define maxlen 10;
+#define MAXLEN 10
 
-void printArr(int arr[], int index){
-	for (int i = index; i < maxlen; ++i)
-	{
-		printf("Fibonacci at index: [%d]\n", arr[i]);
-	}
+void printArr(int arr[], int index) {
+  for (int i = index; i < MAXLEN; ++i) {
+    printf("Fibonacci at index: [%d] = %d\n", i, arr[i]);
+  }
 }
 
-int main(int argc, char const *argv[])
-{
-	srand(time(0));
-	int arrfib[maxlen];
-	arrfib[0] = 0;
-	arrfib[1] = 1;
+int main(int argc, char const *argv[]) {
+  srand(time(0));
+  int arrfib[MAXLEN];
+  arrfib[0] = 0;
+  arrfib[1] = 1;
 
-	for (int i = 2; i < maxlen; ++i)
-	{
-		arrfib[i] = arrfib[i-1] + arrfib[i-2];
-	}
+  for (int i = 2; i < MAXLEN; ++i) {
+    arrfib[i] = arrfib[i - 1] + arrfib[i - 2];
+  }
 
-	int start = rand() % 9;
-	printf("Random start point = %d\n", start);
-	printArr(arrfib, start);
+  int start = rand() % 9;
+  printf("Random start point = %d\n", start);
+  printArr(arrfib, start);
 
-	return 0;
+  return 0;
 }
