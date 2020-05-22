c768101f5514bcd1778b097799e807b6d4063957
fabian <oraze.fabian@icloud.com>
2020-05-22 16:38:20 +0200

    d

---
+++ Blatt_9/9.3/namedev.c
@@ -0,0 +1,43 @@
+#include <linux/cdev.h>
+#include <linux/export.h>
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/kdev_t.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/types.h>
+#include <linux/uaccess.h>
+
+#define DEV_NAME "namedev"
+#define MAJOR_NUMBER 120
+#define MINOR_NUMBER 100
+
+MODULE_AUTHOR("Fabse");
+MODULE_LICENSE("GLP");
+MODULE_DESCRIPTION("Info for module");
+
+dev_t namedev;
+
+static int __init namedev_init(void)
+{
+    printk(KERN_INFO "Namedev started\n");
+
+    namedev = MKDEV(MAJOR_NUMBER, MINOR_NUMBER);
+
+    //check if creation failed
+    if (register_chrdev_region(namedev, 1, DEV_NAME) != 0) {
+        return -1;
+    }
+
+    return 0;
+}
+
+static void __exit namedev_exit(void)
+{
+    printk(KERN_INFO "Namedev ended\n");
+
+    unregister_chrdev_region(namedev, 1);
+}
+
+module_init(namedev_init);
+module_exit(namedev_exit);
